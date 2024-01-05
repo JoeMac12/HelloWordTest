2c09d8fdcef7006b633d1827fb1733b7a80da996
Arman <144450534+JoeMac12@users.noreply.github.com>
2024-01-05 11:38:05 -0400

    add hello world

--- HelloWordTest/Program.cs
+++ HelloWordTest/Program.cs
@@ -11,7 +11,7 @@ namespace HelloWordTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadKey(true);
+            Console.ReadKey();
         }
     }
 }
